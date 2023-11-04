4f2ca3916120b310adc46e6c41ea9763be57ede6
Victor Koome <koomev786@gmail.com>
2023-11-04 09:18:16 +0300

    create a functions that makes x number of divs in rows and columns

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const main = document.getElementById('main')
+const main = document.getElementById("main");
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div')
-    div.classList.add('blah')
-    main.appendChild(div)
-} 
+//a funnction that creates x numbers of divs
 
+function createDivs(x) {
+  for (let rows = 0; rows < x; rows++) {
+    for (let columns = 0; columns < x; columns++) {
+      const div = document.createElement("div");
+      div.classList.add("grid");
+      main.appendChild(div);
+      const heightValue = 600 / x;
+      div.style.height = heightValue + "px";
+      div.style.width = heightValue + "px";
+    }
+  }
+}
+
+createDivs(16);
